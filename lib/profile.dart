2eaf47a1deb887444bb5d9ab39ea173ab4e3ec3a
iRoseM <Rosetmady@gmail.com>
2025-10-03 12:03:29 +0300

    adding IBM Plex Sans Arabic font + nav bar in all pages

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'home.dart'; // عشان تستفيد من AppColors لو تحب
 
 class profilePage extends StatelessWidget {
@@ -6,30 +7,44 @@ class profilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("الملف الشخصي"),
-        backgroundColor: AppColors.primary,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            CircleAvatar(
-              radius: 50,
-              backgroundColor: AppColors.primary,
-              child: Icon(Icons.person, color: Colors.white, size: 50),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "مرحبا بك في ملفك الشخصي 👤",
-              style: TextStyle(
-                fontSize: 18,
+    return Directionality(
+      // عشان النصوص بالعربي تكون RTL
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "الملف الشخصي",
+            style: GoogleFonts.ibmPlexSansArabic(
+              textStyle: const TextStyle(
+                color: Colors.white,
                 fontWeight: FontWeight.w600,
-                color: AppColors.dark,
               ),
             ),
-          ],
+          ),
+          backgroundColor: AppColors.primary,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundColor: AppColors.primary,
+                child: Icon(Icons.person, color: Colors.white, size: 50),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "مرحبا بك في ملفك الشخصي 👤",
+                style: GoogleFonts.ibmPlexSansArabic(
+                  textStyle: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.dark,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
